b3e2cd0299b862be9e71fa65eb5b5801aa27018b
sangwon Oh <ntts7190@gmail.com>
2020-11-05 10:25:21 -0500

    add Search table for shodan search results

--- Project_shodan.py
+++ Project_shodan.py
@@ -1,27 +1,22 @@
 import shodan
-import terminaltables
+from terminaltables import AsciiTable
+
 
 SHODAN_KEY = input("[+]insert shodan key: ")
 api = shodan.Shodan(SHODAN_KEY)
 
+Search_list = []
 
-def  result(com, val):
-    if(com == "add"):
-        for val in list():
-            print(val)
-
-    elif(com == "show"):
-        print("it will show list")
-
-    elif(com == "scan start"):
-        print("for sca start")
-
+def _Table():
+    Search_list.append(['Organization', 'port', 'ip address'])
+    Search_table = AsciiTable(Search_list)
+    print(Search_table.table)
 
 
 try:
     insert_search = input("[+] search: ")
     results = api.search(insert_search)
-    print(results)
+    _Table()
 
 except Exception as e:
     print("error", e)
