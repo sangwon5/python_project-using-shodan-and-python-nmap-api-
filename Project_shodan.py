abdd3d7434880be2836d3712f47b3ead44effd12
sangwon Oh <ntts7190@gmail.com>
2020-11-07 02:24:18 -0500

    made terminaltable for the result and add save the result in result.txt

--- Project_shodan.py
+++ Project_shodan.py
@@ -8,11 +8,23 @@ api = shodan.Shodan(SHODAN_KEY)
 Search_list = []
 Search_list.append(['Organization', 'port', 'ip address'])
 
+_list = {}
+
+
+
+
 try:
     insert_search = input("[+] search: ")
-    results = api.search(insert_search)
+    results = api.search(str(insert_search))
     Search_table = AsciiTable(Search_list)
-    print(Search_table.table)
+    Search_table.inner_row_border = True
 
+    for target_options in results['matches']:
+        _list[target_options['ip_str']] = {target_options['org']}
+        Search_list.append([target_options['org'],target_options['port'],str(target_options['ip_str'])])
+    print(Search_table.table)
+    file = open('result.txt', 'w')
+    file.write(Search_table.table)
+    file.close()
 except Exception as e:
     print("error", e)
